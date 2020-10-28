f666715e5d8f6c52ced26847419e445727b8e9b2
Neeraj Joseph <njoseph@poppulo.com>
2020-10-28 20:49:07 +0000

    Initial commit

--- main.py
+++ main.py
@@ -3,31 +3,36 @@ from PIL import Image, ImageDraw
 
 
 def start():
-
+    # ----------------------------
     # Set values
-    #----------------------------
-    imageFile = 'sample.png'
+    # ----------------------------
+    image_file = 'sample.png'
     # Coordinates, where [0,0] is top left corner
     top_left_corner = [100, 100]  # [x, y]
     bottom_right_corner = [200, 200]  # [x, y]
     # ----------------------------
 
+    # ----------------------------
     # Preview area
-    #----------------------------
-    img = Image.open(imageFile)
+    # ----------------------------
+    img = Image.open(image_file)
 
     top_left_x = top_left_corner[0]
     top_left_y = top_left_corner[1]
     bottom_right_x = bottom_right_corner[0]
     bottom_right_y = bottom_right_corner[1]
-    preview(img, top_left_x, top_left_y, bottom_right_x, bottom_right_y)
+
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((top_left_x, top_left_y, bottom_right_x, bottom_right_y), outline=255)
+    img.save('preview.png')
     # ----------------------------
 
+    # ----------------------------
     # Calculate deviation
     # ----------------------------
-    grayscale_image = Image.open(imageFile).convert('L')
+    grayscale_image = Image.open(image_file).convert('L')
     pixel_array = numpy.array(grayscale_image) / 255.0 # normalize
-    print(f"Image file: {imageFile} , height x width : {pixel_array.shape}")
+    print(f"Image file: {image_file} , height x width : {pixel_array.shape}")
 
     sub_section = pixel_array[top_left_x:bottom_right_x, top_left_y:bottom_right_y]
 
@@ -36,12 +41,6 @@ def start():
     print(f"Deviation: {deviation}")
 
     print('Done')
-
-# Preview area
-def preview(img, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
-    draw = ImageDraw.Draw(img)
-    draw.rectangle((top_left_x, top_left_y, bottom_right_x, bottom_right_y), outline=255)
-    img.save('preview.png')
 
 
 if __name__ == '__main__':
